f7b5635f58e2eb84208a05a5bd6f0e7dc0fc24ad
Phil <pmash2@users.noreply.github.com>
2021-03-06 21:25:35 -0500

    Original scaffolding for project

---
+++ src/pages/mainPage/components/topMenu.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="top-menu">
+      <router-link class="top-menu__btn" v-for="item in items" :to="item.link">{{ item.name }}</router-link>
+      
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top-menu',
+  props: {
+      items: Array,
+  }
+}
+</script>
+
+<style lang="scss">
+    .top-menu {
+        display: flex;
+        flex-flow: row wrap;
+        align-content: center;
+        justify-content: center;
+
+        &__btn {
+            color: greenyellow;
+            margin-left: 10px;
+        }
+
+        &__btn:first-child {
+            color: blueviolet;
+            margin-left: 0;
+        }
+    }
+</style>
---
+++ src/pages/mainPage/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="main-page">
+      <top-menu :items="menuItems"/>
+  </div>
+</template>
+
+<script>
+import topMenu from './components/topMenu'
+
+export default {
+  name: 'main-page',
+  components: {
+      topMenu,
+  },
+  data: function () {
+      return {
+        menuItems: [
+            {
+                link: "/about",
+                name: "About",
+            },
+            {
+                link: "/",
+                name: "Home",
+            },
+            {
+                link: "/contact",
+                name: "Contact",
+            },
+        ]
+      }
+  }
+}
+</script>
+
+<style lang="scss">
+    .main-page {
+        display: flex;
+        flex-flow: row wrap;
+        align-content: center;
+        justify-content: center;
+
+        &__btn {
+            color: greenyellow;
+            margin-left: 10px;
+        }
+
+        &__btn:first-child {
+            color: blueviolet;
+            margin-left: 0;
+        }
+    }
+</style>
