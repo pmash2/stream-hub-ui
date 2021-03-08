21cfef756a418ed6fea672d805afa69d05f05e97
Phil <pmash2@users.noreply.github.com>
2021-03-07 20:41:58 -0500

    Example of passing data to new component

---
+++ src/pages/staticCommandsPage/components/dataTable.vue
@@ -0,0 +1,31 @@
+<template>
+  <table>
+        <thead>
+            <tr>
+                <td>Id</td>
+                <td>Keyword</td>
+                <td>Text</td>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="command in commands" :key="command.Id">
+                <td>{{ command.Id }}</td>
+                <td>{{ command.Keyword }}</td>
+                <td>{{ command.Text }}</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: 'data-table',
+    props: {
+        commands: Array,
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/staticCommandsPage/staticCommands.vue
+++ src/pages/staticCommandsPage/staticCommands.vue
@@ -1,12 +1,35 @@
 <template>
-  <div>
-      <h1>This is the StaticCommands page</h1>
-  </div>
+    <div>
+        <h1>This is the StaticCommands page</h1>
+        <data-table :commands="commands" />
+    </div>
 </template>
 
 <script>
+import dataTable from './components/dataTable.vue'
+
 export default {
-    name: 'static-commands'
+  components: { dataTable },
+    name: 'static-commands',
+    components: {
+        dataTable,
+    },
+    data: function () {
+        return {
+            commands: [
+                {
+                    Id: 1,
+                    Keyword: 'test',
+                    Text: 'Here is the test text',
+                },
+                {
+                    Id: 2,
+                    Keyword: 'test2',
+                    Text: 'Here is the second test text',
+                },
+            ]
+        }
+    }
 }
 </script>
 
