566bfbf637abecfb3eb0a109b66b7e853e3c6625
Phil <pmash2@users.noreply.github.com>
2021-03-15 20:13:36 -0400

    Create chatMessage component

---
+++ src/pages/overlayPage/components/chatMessage.vue
@@ -0,0 +1,18 @@
+<template>
+  <md-list-item>
+    <md-avatar>
+      <img src="@/assets/logo.png" />
+    </md-avatar>
+
+    <span class="md-list-item-text">{{ message }}</span>
+  </md-list-item>
+</template>
+
+<script>
+export default {
+  name: "chat-message",
+  props: {
+    message: String,
+  },
+};
+</script>
